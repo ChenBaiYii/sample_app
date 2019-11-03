1ca582941cdc2f3ad842312ffef2aa140c8a9775
ChenBaiYii <2327279962@qq.com>
2019-11-03 14:23:00 +0800

    Make a basic User model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,7 +23,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', "About | #{@base_title}"
+    assert_select 'title', 'Ruby on Rails'
   end
 
   test 'should get contact' do
